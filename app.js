71f21cf54a0bf8807914e9f82eddb19566687401
Theo Salzmann <dev@on3iro.de>
2018-04-03 14:47:37 +0200

    Add api route project structure

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
+
 const config = require('./config')
+const routes = require('./routes')
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello Sandstorm!')
 })
+
+app.use(routes)
 
 app.listen(config.port, () => {
   console.log(`Today-I-Learned runnging on ${config.port}`)
