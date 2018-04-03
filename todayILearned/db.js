1517d19a0491b8a14903129d91451a5a31bc5ac7
Theo Salzmann <dev@on3iro.de>
2018-04-03 14:21:17 +0200

    Add simple test query

--- todayILearned/db.js
+++ todayILearned/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose()
 
-const db = new sqlite3.Database('data/til.db', (err) => {
+const db = new sqlite3.Database('./data/til.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     return console.log(err.message)
   }
@@ -8,18 +8,18 @@ const db = new sqlite3.Database('data/til.db', (err) => {
   console.log('Connection to database succesfully established.')
 })
 
+const query = `SELECT DISTINCT Name name FROM users ORDER BY name`
+
 db.serialize(() => {
-  db.run("CREATE TABLE IF NOT EXISTS test (info TEST)")
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      throw err
+    }
 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
+    rows.forEach((row) => {
+      console.log(row.name)
+    })
+  })
 })
 
 db.close((err) => {
