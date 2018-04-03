990b4b06b2c36f6fd15746cfb64dcf294af7d632
Theo Salzmann <dev@on3iro.de>
2018-04-03 15:14:03 +0200

    wip

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
+const db = require('../../db')
 
-router.get('/', (req, res, next) => {
-  res.send('this is an article...')
+router.get('/articles', (req, res, next) => {
+  const connection = db.connect()
+
+  const query = `SELECT * FROM articles`
+
+  connection.serialize(() => {
+    connection.all(query, [], (err, rows) => {
+    if (err) {
+      throw err
+    }
+
+      res.json(rows)
+    })
+  })
 })
 
 module.exports = router
