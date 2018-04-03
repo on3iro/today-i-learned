913dad95acefc855799874c260ecb5dadc14aa18
Theo Salzmann <dev@on3iro.de>
2018-04-03 15:15:12 +0200

    basic query is working

--- db/index.js
+++ db/index.js
@@ -1,8 +1,6 @@
 const sqlite3 = require('sqlite3').verbose()
 const path = require('path')
 
-console.log(path.join(__dirname, 'til.db'))
-
 const connect = () => {
   return new sqlite3.Database(path.join(__dirname, 'til.db'), sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
