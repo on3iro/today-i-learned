c28c4d84c88bb790ca5a3eb015cd199fe17cd9b0
Theo Salzmann <dev@on3iro.de>
2018-04-04 08:32:21 +0200

    Add authorName to articles query

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -4,7 +4,14 @@ const db = require('../../db')
 const connection = db.connection
 
 router.get('/articles', (req, res, next) => {
-  const query = `SELECT * FROM articles`
+  const query = `
+    SELECT
+      title,
+      body,
+      users.name as authorName
+    FROM articles
+    INNER JOIN users on users.id = articles.authorId
+  `
 
   connection.serialize(() => {
     connection.all(query, [], (err, rows) => {
