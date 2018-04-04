6b486a67bb7e00408a0b9ad1f6e569efca8a19cb
Theo Salzmann <dev@on3iro.de>
2018-04-04 08:50:43 +0200

    Add some more styling to articles

--- public/src/views/containers/Articles/List.jsx
+++ public/src/views/containers/Articles/List.jsx
@@ -1,23 +1,40 @@
 // @flow
 
 import * as React from 'react'
+import styled from 'styled-components'
 
 import Ul from 'views/components/Ul'
 
+const ArticlesUl = Ul.extend`
+  margin: 0 20px 0 0;
+  border-right: 2px solid #eee;
+  padding-right: 20px;
+  width: 50%;
+`
+
+const Li = styled.li`
+  display: block;
+
+  margin-bottom: 10px;
+  padding: 10px;
+
+  background: ${props => props.theme.white};
+
+  box-shadow: 2px 2px 2px #aaa;
+`
+
 const renderArticles = (articles) => articles.map(article => {
   return (
-    <li>
-      { article.title }, { article.author }
-
-      { article.body }
-    </li>
+    <Li>
+      { article.title }, { article.authorName }
+    </Li>
   )
 })
 
 const List = ({ articles }: Object) => (
-  <Ul>
+  <ArticlesUl>
     { renderArticles(articles) }
-  </Ul>
+  </ArticlesUl>
 )
 
 export default List
